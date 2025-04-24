e30823f958604d1cdea8fbcd61b90543383dc90e
Nifla <fathimanifla1118@gmail.com>
2025-04-24 22:34:12 +0530

    basic js operations

---
+++ script.js
@@ -0,0 +1,40 @@
+
+document.getElementById("demo").innerHTML="hello this is nifla"
+function myfunction(){
+
+    document.getElementById("helo").innerHTML="hello world";
+
+}
+
+
+function nifla(p1,p2){
+
+    return p1*p2
+    
+}
+
+document.getElementById("nif").innerHTML=nifla(5,100);
+document.getElementById("col").style.color="red";
+
+// document.write("<br>this is new world<br>");
+// 
+// document.write(" <br>")
+// document.write(6+3)
+
+window.alert("this is ok")
+
+
+function code(){
+
+    document.getElementById("hi").innerHTML="hi"
+    document.getElementById("me").innerHTML=(5+5);
+}
+
+
+let x=5;
+let y=7;
+
+let total=x+y;
+
+document.getElementById("total1").innerHTML=("total is "+total)
+
